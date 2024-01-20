90c5c82deb5d96e0ff34f93bfdb477ecfe03c292
Tsiangana <pedrozau250@gmail.com>
2024-01-20 21:38:38 +0000

    Primeiro commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	printf("sms1\n");
+	printf("sms2\n");
+	printf("sms3\n");
+	printf("sms4\n");
+	return 0;
+}
