7f006d26167ecbb33191f8153198079d28230e38
Tsiangana <pedrozau250@gmail.com>
2024-01-25 20:32:32 +0000

    Meu sexto dia aprendendo linguagem C dese o zero

---
+++ project_7/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	float m;
+	
+	printf("Insira a nota:\n");
+	scanf("%f", &m);
+	
+	if(m >= 7.0){
+		printf("Aprovado(a)!\n");
+	} else {
+		printf("Reprovado(a)!\n");
+	}   
+	
+}
+
